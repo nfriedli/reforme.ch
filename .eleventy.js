cfa6a8d3aa408c2dddab0b65d755d28b07f4d94e
Nicolas Friedli <nicolas.friedli@gmail.com>
2020-06-27 11:01:06 +0200

    Conseils de sites ;)

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,38 @@
 const htmlmin = require("html-minifier");
 
+
 module.exports = function(eleventyConfig) {
+
+  // https://github.com/hankchizljaw/educationlinks.fyi
+  // Limit feed items and shuffle them so they are random for
+  // each build
+  eleventyConfig.addFilter('shuffle', items => {
+    let currentIndex = items.length;
+    let temporaryValue;
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = items[currentIndex];
+      items[currentIndex] = items[randomIndex];
+      items[randomIndex] = temporaryValue;
+    }
+
+    return items;
+  });
+
+  // A simple limiter
+  eleventyConfig.addFilter('limit', items => {
+    return items.slice(0, 20);
+  });
+
+
+
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
     if( outputPath.endsWith(".html") ) {
       let minified = htmlmin.minify(content, {
--- index.md
+++ index.md
@@ -2,6 +2,10 @@
 title: protestant | réformé
 layout: base.njk
 description: Nom de domaine intéressant à donner aux intéressé·e·s...
+conseils: 
+    - titre: Réseau protestant 
+      url: https://reseau-protestant.ch/
+      description: Une proposition pragmatique pour entretenir la solidarité entre les différents sites du web protestant de Suisse romande. C’est en créant un réseau que nous sommes plus forts!
 ---
 
 *Ecclesia semper reformanda est*, l’Église réformée est toujours à réformer.
@@ -12,3 +16,6 @@ J’avais des idées pour en parler, mais le cœur n’y est plus.
 À défaut, il sera remis en vente en octobre 2020.
 
 Envoyez votre proposition à [nicolasfriedli@protonmail.ch](mailto:nicolasfriedli@protonmail.ch "Contacter Nicolas Friedli")!
+
+
+Quelques sites pour continuer votre navigation (choisis et triés au hasard):
